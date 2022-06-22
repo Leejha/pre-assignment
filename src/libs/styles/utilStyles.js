abb16d2fed5fd1229bb384b9a4e90f60353a2891
이재하 <jhl2619@naver.com>
2022-06-22 18:40:53 +0900

    feat : Add utilStyles

---
+++ src/libs/styles/utilStyles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+import { css } from 'styled-components';
+
+export const FlexColumn = css`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const FlexRow = css`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const FlexJustifyCenter = css`
+  display: flex;
+  justify-content: center;
+`;
+
+export const FlexBetween = css`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const FlexAlignCenter = css`
+  display: flex;
+  align-items: center;
+`;
+
+export const FlexCenter = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
