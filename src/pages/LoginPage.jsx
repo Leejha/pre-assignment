f7da6d7b3440574f89ece393971d983cccffc292
이재하 <jhl2619@naver.com>
2022-06-22 18:54:13 +0900

    feat : Add login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
+import LoginContainer from '../components/login/LoginContainer';
+import OtherContainer from '../components/login/OtherContainer';
 
 function LoginPage() {
   return (
-    <LoginForm>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <button>Login</button>
-    </LoginForm>
+    <Layout>
+      <LoginBox>
+        <LoginContainer />
+        <OtherContainer />
+      </LoginBox>
+    </Layout>
   );
 }
+
+const Layout = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`;
+
+const LoginBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 350px;
+  background-color: #ffff;
+  border: 1px solid #e5e5e5;
+  margin: 0 auto;
+  padding: 0 46px;
+`;
 
 export default LoginPage;
